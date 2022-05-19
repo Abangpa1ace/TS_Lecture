ed159eb7f2e2b4a9d649e9728d3d429adfcc17d9
Abangpa1ace <dyek72731@gmail.com>
2022-05-20 04:16:38 +0900

    프로젝트1 실습

--- basic/example/dropdown-generic.ts
+++ basic/example/dropdown-generic.ts
@@ -1,16 +1,22 @@
-const emails = [
+interface SelectType<T> {
+  value: T;
+  selected: boolean;
+}
+
+const emails: SelectType<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+
+const numberOfProducts: SelectType<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+function createDropdownItem<T>(item: SelectType<T>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -20,7 +26,7 @@ function createDropdownItem(item) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
